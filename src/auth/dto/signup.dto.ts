96fbcd4284157c8948c9b15dcc9ff0e4a184ba89
shruthiakkal <samakkal93@gmail.com>
2026-02-12 09:26:57 +0530

    signup dto

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,11 +1,12 @@
 import {
   IsEmail,
   isEmail,
+  IsNotEmpty,
   IsString,
   Matches,
   MaxLength,
   MinLength,
-} from 'class-validator';
+} from "class-validator";
 
 export class SignUpDto {
   @IsString()
@@ -15,10 +16,12 @@ export class SignUpDto {
   @IsEmail()
   email: string;
 
+  @IsNotEmpty()
+  @IsString()
   @MinLength(8)
   @MaxLength(72)
   @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/, {
-    message: 'Password must include uppercase, lowercase, and a number',
+    message: "Password must include uppercase, lowercase, and a number",
   })
   password: string;
 }
